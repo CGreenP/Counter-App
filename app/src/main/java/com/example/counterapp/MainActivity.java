436958bd57c013f4d2ddb1904b1b34b321ad9d7e
Chayan Pal <chayan110201@gmail.com>
2024-05-17 20:13:21 +0530

    Issue #2 On device rotation the counter number goes to 0 fixed.

--- app/src/main/java/com/example/counterapp/MainActivity.java
+++ app/src/main/java/com/example/counterapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,7 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     TextView welcome_text,counter_text;
     Button btn;
-    int counter = 0;
+    private int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +37,17 @@ public class MainActivity extends AppCompatActivity {
                 counter_text.setText(""+increaseCounter());
             }
         });
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt("count");
+            counter_text.setText("" + counter);
+        }
     }
     public int increaseCounter(){
         return ++counter;
+    }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("count",counter);
     }
 }
